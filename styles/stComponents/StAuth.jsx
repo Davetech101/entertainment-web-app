c3fe06213d6db245ab117413f18680bc5bac0b29
Ogaranya <enomahog@gmail.com>
2022-09-06 21:13:59 +0100

    initial auth styles

---
+++ styles/stComponents/StAuth.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const StAuth = styled.form`
+  padding: 0 2.4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 3.2rem;
+    margin-bottom:4rem;
+  }
+
+  .card {
+    background: #161d2f;
+    margin-top: 6rem;
+    padding: 2.4rem;
+  }
+
+  input {
+    display: block;
+    border-bottom:1px solid #ffffff82;
+  }
+`;
+export default StAuth;
