c408ad705c62ccf12f2167904f21366814d2af4d
Ogaranya <enomahog@gmail.com>
2022-09-07 07:22:06 +0100

    futher work on sign up UI

--- styles/stComponents/StAuth.jsx
+++ styles/stComponents/StAuth.jsx
@@ -5,21 +5,46 @@ const StAuth = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100vw;
 
   h2 {
     font-size: 3.2rem;
-    margin-bottom:4rem;
+    margin-bottom: 1rem;
   }
 
   .card {
     background: #161d2f;
-    margin-top: 6rem;
+    margin-top: 3rem;
     padding: 2.4rem;
+    width: 90%;
   }
 
   input {
     display: block;
-    border-bottom:1px solid #ffffff82;
+    border-bottom: 1px solid #ffffff82;
+    width: 100%;
+    padding: 2rem;
+    color:#fff;
+  }
+
+  button {
+    background: #fc4747;
+    border-radius: 6px;
+    border:none;
+    display:block;
+    width:70%;
+    margin:3rem auto 2rem;
+    padding:1rem 3rem;
+    color:#fff;
+  }
+
+  aside{
+    font-size:1.5rem;
+    text-align:center;
+  }
+
+  a{
+    color:#FC4747;
   }
 `;
 export default StAuth;
