935087c231e53345a4ada1d3b37a82cdeada8e21
Ogaranya <enomahog@gmail.com>
2022-09-11 07:35:23 +0100

    adjust trending layout

--- styles/stComponents/StTrending.jsx
+++ styles/stComponents/StTrending.jsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const StTrending = styled.div`
   margin-top: 2.4rem;
+  animation: animate-2 2s;
+  .cont::-webkit-scrollbar {
+    display: none;
+  }
 
   h3 {
     font-size: 2rem;
@@ -19,7 +23,8 @@ const StTrending = styled.div`
   .img {
     position: relative;
     height: 14rem;
-    width:24rem;
+    width:70vw;
+    border-radius:1rem;
   }
 
   .info {
@@ -59,6 +64,80 @@ const StTrending = styled.div`
     width:3.2rem;
     height:3.2rem;
   }
+
+  @media only screen and (min-width: 420px) {
+    .img {
+      height: 16rem;
+      width: 65vw;
+    }
+  }
+
+  @media only screen and (min-width: 530px) {
+    .img {
+      height: 16rem;
+      width: 60vw;
+    }
+  }
+
+  @media only screen and (min-width: 600px) {
+    .img {
+      height: 16rem;
+      width: 50vw;
+    }
+  }
+
+  @media only screen and (min-width: 700px) {
+    .img {
+      height: 16rem;
+      width: 45vw;
+    }
+  }
+
+  @media only screen and (min-width: 800px) {
+    padding-left:10rem;
+    margin-top:5rem;
+
+    .img {
+      height: 16rem;
+      width: 40vw;
+    }
+
+    h3 {
+      font-size:3.5rem;
+    }
+  }
+
+  @media only screen and (min-width: 900px) {
+    .img {
+      height: 16rem;
+      width: 35vw;
+    }
+  }
+
+  @media only screen and (min-width: 1000px) {
+    .img {
+      height: 20rem;
+      width: 35vw;
+    }
+  }
+
+  @keyframes animate-2{
+    0%{
+      transform:translate(100rem);
+      opacity:0;
+    }
+
+    80%{
+      transform:translate(-5rem);
+      opacity:.8;
+    }
+
+    100%{
+      ransform:translate(0);
+      opacity:1;
+    }
+  }
+
 `;
 
 export default StTrending;
