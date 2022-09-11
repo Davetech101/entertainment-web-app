9f6befd16deef77a511b271dd49dc674261147e2
Ogaranya <enomahog@gmail.com>
2022-09-11 07:30:37 +0100

    update for responsive UI

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,37 +14,81 @@ const Header = () => {
   const router = useRouter();
   return (
     <StHeader>
-      <MovieMain />
+      <div className="mobile">
+        <MovieMain />
 
-      <nav className="nav">
-        <Link passHref href="/">
-          <span>
-            <HomeNI fill={router.asPath === "/" ? "#fff" : "#5A698F"} />
-          </span>
-        </Link>
+        <nav className="nav">
+          <Link passHref href="/">
+            <span>
+              <HomeNI fill={router.asPath === "/" ? "#fff" : "#5A698F"} />
+            </span>
+          </Link>
 
-        <Link passHref href="/movies">
-          <span>
-            <MoviesNI fill={router.asPath === "/movies" ? "#fff" : "#5A698F"} />
-          </span>
-        </Link>
+          <Link passHref href="/movies">
+            <span>
+              <MoviesNI
+                fill={router.asPath === "/movies" ? "#fff" : "#5A698F"}
+              />
+            </span>
+          </Link>
 
-        <Link passHref href="/tv-series">
-          <span>
-            <TvNI fill={router.asPath === "/tv-series" ? "#fff" : "#5A698F"} />
-          </span>
-        </Link>
+          <Link passHref href="/tv-series">
+            <span>
+              <TvNI
+                fill={router.asPath === "/tv-series" ? "#fff" : "#5A698F"}
+              />
+            </span>
+          </Link>
 
-        <Link passHref href="/bookmarked">
-          <span>
-            <BookmarkNI
-              fill={router.asPath === "/bookmarked" ? "#fff" : "#5A698F"}
-            />
-          </span>
-        </Link>
-      </nav>
+          <Link passHref href="/bookmarked">
+            <span>
+              <BookmarkNI
+                fill={router.asPath === "/bookmarked" ? "#fff" : "#5A698F"}
+              />
+            </span>
+          </Link>
+        </nav>
 
-      <Image src={avatar} alt="Avatar" width="30px" height="30px" />
+        <Image src={avatar} alt="Avatar" width="30px" height="30px" />
+      </div>
+
+      <div className="desktop">
+        <MovieMain />
+
+        <nav className="nav">
+          <Link passHref href="/">
+            <span>
+              <HomeNI fill={router.asPath === "/" ? "#fff" : "#5A698F"} />
+            </span>
+          </Link>
+
+          <Link passHref href="/movies">
+            <span>
+              <MoviesNI
+                fill={router.asPath === "/movies" ? "#fff" : "#5A698F"}
+              />
+            </span>
+          </Link>
+
+          <Link passHref href="/tv-series">
+            <span>
+              <TvNI
+                fill={router.asPath === "/tv-series" ? "#fff" : "#5A698F"}
+              />
+            </span>
+          </Link>
+
+          <Link passHref href="/bookmarked">
+            <span>
+              <BookmarkNI
+                fill={router.asPath === "/bookmarked" ? "#fff" : "#5A698F"}
+              />
+            </span>
+          </Link>
+        </nav>
+
+        <Image src={avatar} alt="Avatar" width="30px" height="30px" />
+      </div>
     </StHeader>
   );
 };
